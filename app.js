95a231a21c13903f1fabaec6994676c4c5ecaaa2
Emelike Adaeze <emelikepatience961@gmail.com>
2022-12-16 02:14:32 +0100

    added display note feature

--- app.js
+++ app.js
@@ -1,57 +1,116 @@
 
-var form = document.querySelector('#form-ccontainer')
-var todoTitle = document.querySelector('#to-do-title')
-var todoText = document.querySelector('#to-do-text')
-var formButton = document.querySelector('#form-button')
-var come = 'do';
+let form = document.querySelector('#form-ccontainer')
+let toDoList = document.querySelector('#to-do-list')
+let todoTitle = document.querySelector('#to-do-title')
+let todoText = document.querySelector('#to-do-text')
+let formButton = document.querySelector('#form-button')
+let listPlaceholder = document.querySelector('.list-placeholder')
+let notes = []
 
 
-document.body.addEventListener('click', event =>{
+document.body.addEventListener('click', event => {
     handleForm(event)
-    console.log('hello')
 })
 
-function openForm(event){
-  
-    form.classList.add('form-open');
-    window.todoTitle.style.display = "flex";
-    window.formButton.style.display = "block";
+function openForm(event) {
+    
+        form.classList.add('form-open');
+        todoTitle.style.display = "block";
+        formButton.style.display = "block";
+
+    
 
 
 }
 
-function closeForm(event){
-    console.log(come)
+function closeForm(event) {
+ 
+        form.classList.remove('form-open');
+        todoTitle.style.display = "none";
+        formButton.style.display = "none";
 
-    form.classList.remove('form-open');
-    window.todoTitle.style.display = "none";
-    window.formButton.style.display = "none";
+    displayNote()
 }
 
 
 
-function handleForm(event){
+function handleForm(event) {
 
     const isFormClicked = form.contains(event.target)
-     
-     if(isFormClicked){
-         openForm(event)
+
+   let title = todoTitle.value;
+   let text = todoText.value;
+    const hasNote = title || text;
+    console.log(hasNote)
+
+
+    if (isFormClicked) {
+        openForm(event)
+
+    } else if (hasNote){
+        addNote({title,  text})
+    }
     
-    }else{
+    else {
         closeForm(event)
     }
-   
-    
+
+
+}
+
+function addNote({title, text}){
+
+    const newNote = {
+       title,
+       text,
+       color: 'white',
+       id : notes.length > 0 ? notes[notes.length - 1].id+ 1 : 1
+    };
+        console.log(newNote)
+
+    notes = [...notes, newNote]
+    closeForm()
+}
+
+
+function displayNote(){
+let hasNote = notes.length > 0;
+listPlaceholder.style.display = hasNote ? 'none' : 'flex'
+
+toDoList.innerHTML = notes.map(note =>`
+<div style = "background: ${note.color};" class = "note" data-id="${note.id}">
+    <div class = "${note.title && 'note-title'}">${note.title}</div>
+    <div class="note-text">${note.text}</div>
+    <div class="toolbar-container">
+      <div class="toolbar">
+        <img class="toolbar-color" data-id=${note.id} src="https://icon.now.sh/palette">
+        <img class="toolbar-delete" data-id=${note.id}  src="https://icon.now.sh/delete">
+      </div>
+    </div>
+    </div>
+
+`)
 }
 
 
 
-// function openForm(event){
-//     const text = todoText;
-//     const title = todoTitle;
-//     const button = formButton
 
-//     form.classList.add('form-open');
-//     title.style.display = "block"
-//     button.style.display = "block"
+
+// function addList(){
+//  todoTitle = todoTitle.value;
+//  console.log(todoTitle)
+//  todoText = todoText.value;
+//     const hasNote = todoTitle || todoTitle;
+
+// if(hasNote){
+//     this.addNote()
 // }
+//     console.log(hasNote)
+
+// }
+
+
+// if(todoTitle && todoText){
+//     addList()
+// }
+//     ()
